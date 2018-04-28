984339d98842f17942683c009d50036108b8fd66
rockwithyou <rockwithyou@126.com>
2018-04-28 11:37:59 +0800

    Modify Pom.

--- src/main/java/com/hisun/util/BeanMapper.java
+++ src/main/java/com/hisun/util/BeanMapper.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018. Hunan Hisun Union Information Technology Co, Ltd. All rights reserved.
+ * http://www.hn-hisun.com
+ * 注意:本内容知识产权属于湖南海数互联信息技术有限公司所有,除非取得商业授权,否则不得用于商业目的.
+ */
+
 package com.hisun.util;
 
 import com.google.common.collect.Lists;
@@ -6,11 +12,6 @@ import org.dozer.DozerBeanMapper;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * 简单封装Dozer, 实现深度转换Bean<->Bean的Mapper.
- *
- * @author Jason
- */
 public class BeanMapper {
 
     /**
